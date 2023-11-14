8b9b989eaf6180bbd425be122b4a54fcbf52d47a
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2023-11-14 21:25:14 +0900

    Add rewrites setting to Next.js server.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const DELTA_SHARING_SERVER_URL = process.env.DELTA_SHARING_SERVER_IP_ADDR;
+
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        //	source: '/api/catalog/:path*',
+        source: '/api/sharing/:path*',
+        //	destination: `${DELTA_SHARING_SERVER_IP_ADDR}/catalog/:path*`,
+        destination: `${DELTA_SHARING_SERVER_URL}/sharing/:path*`,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
