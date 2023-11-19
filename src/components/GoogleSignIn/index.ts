da54613667a8c9718d8dd09dea0f1ac25f1f2e26
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2023-11-20 04:44:42 +0900

    Remove deprecated components.

--- src/components/GoogleSignIn/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @fileoverview Exports GoogleSignIn.
- * @copyright Shingo OKAWA 2023
- */
-export { default as GoogleSignIn } from './GoogleSignIn';
-export type { Props as LogoProps } from './GoogleSignIn';
