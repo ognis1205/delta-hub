c5dedaa30bc381b2ececa169de913b77da94e1b8
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2023-11-21 05:32:42 +0900

    Implement HomeButton

---
+++ src/atoms/HomeButton/index.tsx
@@ -0,0 +1,37 @@
+/**
+ * @fileoverview Defines Home Button atom.
+ * @copyright Shingo OKAWA 2023
+ */
+'use client';
+
+import { Image, ImageProps, Link } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { FC } from 'react';
+
+export type Size = '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | 'full';
+
+export type Props = Omit<ImageProps, keyof Pick<ImageProps, 'boxSize'>> & {
+  size?: Size;
+};
+
+const sizes = {
+  '2xs': '1rem',
+  'xs': '1.5rem',
+  'sm': '2rem',
+  'md': '3rem',
+  'lg': '4rem',
+  'xl': '6rem',
+  '2xl': '8rem',
+  'full': '100%',
+} satisfies Record<Size, string>;
+
+export const Component: FC<Props> = ({
+  size = 'md',
+  ...props
+}: Props) => (
+  <Link as={NextLink} href={'/'}>
+    <Image {...props} src={'/images/logo.png'} h={sizes[size]} alt={'Home button'} />
+  </Link>
+);
+
+Component.displayName = 'HomeButton';
---
+++ src/stories/atoms/HomeButton.stories.tsx
@@ -0,0 +1,42 @@
+/**
+ * @fileoverview Defines @/atoms/HomeButton story.
+ * @copyright Shingo OKAWA 2023
+ */
+import { Component as HomeButton } from '@/atoms/HomeButton';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'HomeButton',
+  component: HomeButton,
+} satisfies Meta<typeof HomeButton>;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+  render: (args) => <HomeButton {...args} />,
+};
+
+export const SizeSmall: Story = {
+  args: {
+    size: 'sm',
+  },
+  render: (args) => <HomeButton {...args} />,
+};
+
+export const SizeMedium: Story = {
+  args: {
+    size: 'md',
+  },
+  render: (args) => <HomeButton {...args} />,
+};
+
+export const SizeLarge: Story = {
+  args: {
+    size: 'lg',
+  },
+  render: (args) => <HomeButton {...args} />,
+};
+
+export default meta;
